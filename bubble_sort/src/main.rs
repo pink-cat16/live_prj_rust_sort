ddf4ae48a949d68982ad88a3e91dc6d723b1976e
DJ. Harris <apparitiondj@outlook.com>
2023-12-26 13:27:59 -0500

    Add cocktial sort

--- bubble_sort/src/main.rs
+++ bubble_sort/src/main.rs
@@ -6,11 +6,21 @@ fn main() {
     let max =  support::get_i32("Max value: ");
 
     if num_items > 0 {
-        let mut sample_vec = support::make_random_vec(num_items, max);
-        support::print_vec(&sample_vec, 30i32);
-        sorts::bubble_sort(&mut sample_vec);
-        support::print_vec(&sample_vec, 30i32);
-        support::check_sorted(&sample_vec);
+        let mut bubble_vec = support::make_random_vec(num_items, max);
+        let mut cocktail_vec = bubble_vec.clone();
+
+        print!("\nBubble sort pass...\n");
+        support::print_vec(&bubble_vec, 30i32);
+        sorts::bubble_sort(&mut bubble_vec);
+        support::print_vec(&bubble_vec, 30i32);
+        support::check_sorted(&bubble_vec);
+
+        print!("\n\nCocktail shaker sort pass\n");
+        support::print_vec(&cocktail_vec, 30i32);
+        sorts::cocktail_sort(&mut cocktail_vec);
+        support::print_vec(&cocktail_vec, 30i32);
+        support::check_sorted(&cocktail_vec);
+
     } else {
         print!("Vector size ({}) has to be greater than zero", num_items);
     }
--- bubble_sort/src/sorts.rs
+++ bubble_sort/src/sorts.rs
@@ -1,19 +1,68 @@
 // Sort routines, for i32 vectors.
 
+
+// Process vector bottom up.
+fn bubble_up(vec: &mut Vec<i32>) -> bool {
+
+    let mut sorting: bool = false;
+
+    for i in 1usize..vec.len() {
+        if vec[i] < vec[i - 1usize] {
+            sorting = true;
+            let swap = vec[i];
+            vec[i] = vec[i - 1usize];
+            vec[i - 1usize] = swap;
+        }
+    }
+
+    sorting
+}
+
+
+// Process vector bottom up.
+fn bubble_down(vec: &mut Vec<i32>) -> bool {
+
+    let mut sorting: bool = false;
+
+    for i in (0usize..(vec.len() - 1usize)).rev() {
+        if vec[i] > vec[i + 1usize] {
+            sorting = true;
+            let swap = vec[i];
+            vec[i] = vec[i + 1usize];
+            vec[i + 1usize] = swap;
+        }
+    }
+
+    sorting
+}
+
+
 // Use bubble sort to sort the vector.
 pub(crate) fn bubble_sort(vec: &mut Vec<i32>) {
 
     let mut sorting: bool = true;
+    let mut pass_cnt: u32 = 0;
 
     while sorting {
-        sorting = false;
-        for i in 1usize..vec.len() {
-            if vec[i] < vec[i - 1usize] {
-                sorting = true;
-                let swap = vec[i];
-                vec[i] = vec[i - 1usize];
-                vec[i - 1usize] = swap;
-            }
-        }
+        sorting = bubble_up(vec);
+        pass_cnt = pass_cnt + 1u32;
     }
+    print!("Bubble pass count: {}\n", pass_cnt);
+}
+
+
+// bi-directional take on the bubble sort, non-optimized version.
+pub(crate) fn cocktail_sort(vec: &mut Vec<i32>) {
+
+    let mut sorting: bool = true;
+    let mut pass_cnt: u32 = 0;
+
+    while sorting {
+        sorting = bubble_up(vec);
+        if sorting {
+            sorting = bubble_down(vec);
+        }
+        pass_cnt = pass_cnt + 1u32;
+    }
+    print!("cocktail pass count: {}\n", pass_cnt);
 }
