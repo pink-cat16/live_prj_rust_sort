1b0229fcd50d4c1f82dd7e4b6420177e0eca4257
DJ. Harris <apparitiondj@outlook.com>
2024-05-24 12:11:40 -0400

    Switch to VSC

--- quick_sort/src/sorts.rs
+++ quick_sort/src/sorts.rs
@@ -1,1 +1,11 @@
 // Sort routines, for i32 vectors.
+
+fn partion(vec: &mut [i32]) -> usize {
+    0
+}
+
+pub(crate) fn quicksort(vec: &mut [i32]) {
+    if vec.len() > 1usize {
+        let pivot = partion(vec);
+    }
+}
